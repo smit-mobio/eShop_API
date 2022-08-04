7b8c31fed8250f66f5b62a5f5dc57db2479dd89f
smit <smitd.mobio@gmail.com>
2022-08-04 16:52:52 +0530

    used dao_handler in user.py

--- apps/users/user.py
+++ apps/users/user.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Response, status
-from database.database import db
 import apps.users.schema as schema
 import database.models as models
+from database.models import Data
+from utils import common_function
+from database.dao import dao_handler
 from werkzeug.security import generate_password_hash
 from datetime import datetime
 
@@ -9,13 +11,14 @@ router = APIRouter(prefix='/users', tags=['Users'])
 
 @router.get('/', response_model=list[schema.UserSchema])
 def get_all_users():
-    all_users = db.query(models.User).all()
+    all_users = dao_handler.user_dao.get_all()
     return all_users
 
 @router.get('/info_user/{id}')
-def get_user(id:int):
-    user = db.query(models.User).filter_by(id=id).first()
+def get_user(id:int, response:Response):
+    user = dao_handler.user_dao.get_by_id(id)
     if not user:
+        response.status_code = status.HTTP_404_NOT_FOUND
         return {'error':'User you are looking for is not exists!'}
     return user
 
@@ -23,31 +26,29 @@ def get_user(id:int):
 def create_user(user:schema.UserSchema):
     has_password = generate_password_hash(user.password, method="sha256")
     new_user = models.User(first_name = user.first_name, last_name = user.last_name, email=user.email, password = has_password, username = common_function.create_username(user.email), created_on = datetime.now(), phone = user.phone)
-    db.add(new_user)
-    db.commit()
-    new_user = db.query(models.User).filter_by(username = new_user.username).first()
+    Data.add(new_user)
+    new_user = dao_handler.user_dao.get_by_id(new_user.id)
     return {'new_user': new_user}
 
 
 @router.patch('/update_user/{id}/')
 def update_user(id:int, user:schema.UserSchema):
-    user_to_update = db.query(models.User).filter_by(id = id).first()
+    user_to_update = dao_handler.user_dao.get_by_id(id)
     if not user_to_update:
         return {'error':'User you are looking for is not exists!'}
     user_to_update.first_name = user.first_name
     user_to_update.last_name = user.last_name
     user_to_update.phone = user.phone
     user_to_update.update_on = datetime.now()
-    db.commit()
+    Data.commit()
     return {'message':'User profile is successfully updated!'}
 
 @router.delete('/delete_user/{id}')
 def delete_user(id:int, response:Response):
-    user_to_delete = db.query(models.User).filter_by(id=id).first()
+    user_to_delete = dao_handler.user_dao.get_by_id(id)
     if not user_to_delete:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {'error':'User you are looking for is not exists!'}
-    db.delete(user_to_delete)
-    db.commit()
+    Data.delete(user_to_delete)
     response.status_code = status.HTTP_200_OK
     return {'success':'User is deleted successfully!'}
