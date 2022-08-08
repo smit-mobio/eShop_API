e4c2b524e1c4fc89ded649fe42747ac01a8e1421
smit <smitd.mobio@gmail.com>
2022-08-08 10:13:38 +0530

    removed fake_user_db and updated get_user function in jwt_handler

--- apps/authentication/jwt_handler.py
+++ apps/authentication/jwt_handler.py
@@ -1,27 +1,17 @@
 from datetime import timedelta, datetime
 from jose import JWTError, jwt
-import time
 from decouple import config
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import Depends, status, HTTPException
-from apps.authentication.schema import TokenData, Token, UserInDB, UserSchema
+from apps.authentication.schema import TokenData, Token
+from apps.users.schema import UserSchema, UserActiveSchema
 from database.dao import dao_handler
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token/")
 JWT_SECRET = config('JWT_SECRET_KEY')
 JWT_ALGORITHM = config('JWT_ALGORITHM')
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
 
 
 def verify_password(plain_password, hashed_password):
@@ -35,19 +25,14 @@ def authenticate_user(email: str, password: str):
     user = dao_handler.user_dao.get_by_email(email)
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, user.password):
         return False
     return user
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-def token_response(token: str):
-    return {
-        "access_token": token
-    }
+def get_user(email: str):
+    if email in [i.email for i in dao_handler.user_dao.get_all()]:
+        user_dict = dao_handler.user_dao.get_by_email(email)
+        return UserActiveSchema(**user_dict.__dict__)
 
 # function used for signing the JWT string
 def create_access_token(data: dict, expires_delta: timedelta):
@@ -60,6 +45,7 @@ def create_access_token(data: dict, expires_delta: timedelta):
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
     return encoded_jwt
 
+
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -68,19 +54,19 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
     )
     try:
         payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
+        email = payload.get("sub")
+        if email is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        token_data = TokenData(email=email)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(email=token_data.email)
     if user is None:
         raise credentials_exception
     return user
 
-def get_current_active_user(current_user: UserSchema = Depends(get_current_user)):
-    if current_user.disabled:
+def get_current_active_user(current_user: UserActiveSchema = Depends(get_current_user)):
+    if not current_user.is_active:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
