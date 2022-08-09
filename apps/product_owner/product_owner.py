4f363f37c490934cc1eac29e624bd0da7f0d1ddb
smit <smitd.mobio@gmail.com>
2022-08-09 11:10:24 +0530

    added dependencies in APIRouter class of product_owner

--- apps/product_owner/product_owner.py
+++ apps/product_owner/product_owner.py
@@ -1,3 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from apps.authentication.auth import get_current_active_user
+from database.models import User
 
-router = APIRouter(prefix='/auth/user/product_owner', tags=['Product-Owner'])
+router = APIRouter(prefix='/auth/user/product_owner', tags=['Product-Owner'], dependencies=[Depends(get_current_active_user)])
