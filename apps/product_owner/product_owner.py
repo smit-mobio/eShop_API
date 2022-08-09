4be32d43b4936378ad6406189e275ab553a89be8
smit <smitd.mobio@gmail.com>
2022-08-09 11:03:38 +0530

    prefix updated in APIRouter class in product_owner's api

--- apps/product_owner/product_owner.py
+++ apps/product_owner/product_owner.py
@@ -1,3 +1,3 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix='auth/user/product_owner/', tags=['Product-Owner'])
+router = APIRouter(prefix='/auth/user/product_owner', tags=['Product-Owner'])
