23fbc9ad3a1385f62c17991043213e7d89b22bbd
Smit Dudhat <smitd.mobio@gmail.com>
2022-08-17 15:19:37 +0530

    created all_products method in customer's api

--- apps/customer/customer.py
+++ apps/customer/customer.py
@@ -1,5 +1,10 @@
-from msilib.schema import AppId
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from apps.authentication.jwt_handler import get_current_active_user
-
+from utils.decorators import only_customer
+from database.dao import dao_handler
 router = APIRouter(prefix='/auth/user/customer', tags=['Customer'], dependencies=[Depends(get_current_active_user)])
+
+@router.get('/products/')
+@only_customer()
+def all_products(response:Response, user = Depends(get_current_active_user)):
+    products = dao_handler
